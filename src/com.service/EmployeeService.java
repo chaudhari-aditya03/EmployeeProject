7e04a5b6170157d8359cadf32f75df5c3db62107
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-07-14 11:04:20 +0530

    Update EmployeeService.java

--- src/com.service/EmployeeService.java
+++ src/com.service/EmployeeService.java
@@ -1,1 +1,22 @@
+package com.Service;
+import com.Dao.EmployeeDao;
+public class EmployeeService {
+	
+	
+	
+	public void InsertEmployee() throws Exception
+	{
+		EmployeeDao e1 = new EmployeeDao();
+		e1.InsertEmployee();
+	}
+	public void ViewEmployee() throws Exception {
+		EmployeeDao e2 = new EmployeeDao();
+		e2.ViewEmployee();
+	}
+	public void DeleteEmployee() throws Exception
+	{
+		EmployeeDao e3 = new EmployeeDao();
+		e3.DeleteEmployee();
+	}
+}
 
